f1fcdfd40b1bcce190f2207ef9bb53957397f6f7
blue <bluemethyst@gmail.com>
2024-09-09 17:51:33 +1200

    oopswo

--- cloudflare-proxy/index.js
+++ cloudflare-proxy/index.js
@@ -4,8 +4,8 @@
     addEventListener('fetch', (event) => {
         event.respondWith(handleRequest(event))
     })
-    var prodDomain = 'https://postmaster.bluemethyst.dev'
-    var domain = '*'
+    var domain = 'https://postmaster.bluemethyst.dev'
+    var temp = '*'
     async function handleRequest(event) {
         const url = new URL(event.request.url)
         if (url.pathname === '/track' && url.searchParams.has('tracking_reference')) {
@@ -19,6 +19,7 @@
             headers.set('Access-Control-Allow-Origin', domain)
             headers.set('Access-Control-Allow-Methods', 'GET')
             headers.set('Access-Control-Allow-Headers', 'Content-Type')
+            console.log(event.request.headers.get('cf-connecting-ip') + ' contacted /track')
             return new Response(response.body, {
                 status: response.status,
                 headers
@@ -34,11 +35,13 @@
             headers.set('Access-Control-Allow-Origin', domain)
             headers.set('Access-Control-Allow-Methods', 'GET')
             headers.set('Access-Control-Allow-Headers', 'Content-Type')
+            console.log(event.request.headers.get('cf-connecting-ip') + ' contacted /image')
             return new Response(response.body, {
                 status: response.status,
                 headers
             })
         } else {
+            console.log(event.request.headers.get('cf-connecting-ip') + ' contacted no endpoint')
             return new Response(JSON.stringify({ error: 'Not Found' }), {
                 status: 404
             })
